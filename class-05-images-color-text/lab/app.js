cd1e27dc2c82b8b17eb0400e5f0bedb3d0fc0284
Khoa Huynh <khoawin92@gmail.com>
2018-01-20 11:55:53 -0800

    problem_4

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -54,10 +54,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  var sum = a + b + c;
-  var multiply = a * b * c;
-  var array = [sum, multiply, a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply + '.'];
-  return array;
   //eslint-disable-line
 }
 
